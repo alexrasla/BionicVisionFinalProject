2f45eb4367859b451251c131e2985079b7ec06b3
Alex Rasla <alexrasla@169-231-210-232.wireless.ucsb.edu>
2021-12-03 11:57:00 -0800

    init num effective and bug

--- swarm_helpers.py
+++ swarm_helpers.py
@@ -18,6 +18,18 @@ import implant_helpers as imp
 
 # The error function must accept a tuple of parameter values `params` first, but
 # can have other input variables like `features` and `targets`
+def get_init_num_effective(swarm_positions, electrode_size, bounds, model):
+
+    min_n_eff = math.inf
+    for electrode_positions in swarm_positions:
+        
+        implant = imp.build_implant(electrode_positions, electrode_size)
+        n_eff = imp.get_num_effective(implant, model)
+        
+        if min_n_eff > n_eff:
+          min_n_eff = n_eff
+          
+    return min_n_eff
 
 def loss_func_basic(swarm_positions, electrode_size, bounds, model):
     
@@ -28,7 +40,7 @@ def loss_func_basic(swarm_positions, electrode_size, bounds, model):
         n_eff = imp.get_num_effective(implant, model)
         scores.append(implant.n_electrodes - n_eff)
           
-    return np.array(scores), n_eff
+    return np.array(scores)
 
 def loss_func_too_close(swarm_positions, electrode_size, bounds, model):
     PENALTY_DISTANCE = electrode_size * 2
@@ -40,17 +52,15 @@ def loss_func_too_close(swarm_positions, electrode_size, bounds, model):
         # print('electrode', electrode_positions)
         implant = imp.build_implant(electrode_positions, electrode_size)
         n_eff = imp.get_num_effective(implant, model)
-        scores.append(implant.n_electrodes - n_eff)
         
         coords = newXYArray(electrode_positions)
         indexes = distance_penalty(coords, PENALTY_DISTANCE, less_than=True)
         num_too_close = len(indexes)
 
         score = (implant.n_electrodes - n_eff) + (0.5 * num_too_close)
-        # print('score', score)
         scores.append(score)
     
-    return np.array(scores), n_eff
+    return np.array(scores)
   
 def loss_func_too_far(swarm_positions, electrode_size, bounds, model):
   PENALTY_DISTANCE = (electrode_size * 2)
@@ -61,7 +71,6 @@ def loss_func_too_far(swarm_positions, electrode_size, bounds, model):
       
       implant = imp.build_implant(electrode_positions, electrode_size)
       n_eff = imp.get_num_effective(implant, model)
-      scores.append(implant.n_electrodes - n_eff)
       
       coords = newXYArray(electrode_positions)
       indexes = distance_penalty(coords, PENALTY_DISTANCE, less_than=False)
@@ -72,7 +81,7 @@ def loss_func_too_far(swarm_positions, electrode_size, bounds, model):
       # print('score', score)
       scores.append(score)
   
-  return np.array(scores), n_eff
+  return np.array(scores)
 
 def loss_func_dist_fovea(swarm_positions, electrode_size, bounds, model):
   
@@ -82,7 +91,6 @@ def loss_func_dist_fovea(swarm_positions, electrode_size, bounds, model):
       
       implant = imp.build_implant(electrode_positions, electrode_size)
       n_eff = imp.get_num_effective(implant, model)
-      scores.append(implant.n_electrodes - n_eff)
       
       coords = newXYArray(electrode_positions)
       zero = [(0,0)]
@@ -94,7 +102,7 @@ def loss_func_dist_fovea(swarm_positions, electrode_size, bounds, model):
       # print('score', score)
       scores.append(score)
   
-  return np.array(scores), n_eff
+  return np.array(scores)
 
 def loss_func_convex_hull(swarm_positions, electrode_size, bounds, model):
   # Electrode positions
@@ -103,7 +111,6 @@ def loss_func_convex_hull(swarm_positions, electrode_size, bounds, model):
       
       implant = imp.build_implant(electrode_positions, electrode_size)
       n_eff = imp.get_num_effective(implant, model)
-      scores.append(implant.n_electrodes - n_eff)
       
       coords = newXYArray(electrode_positions)
       coords = np.array(coords)
@@ -113,8 +120,7 @@ def loss_func_convex_hull(swarm_positions, electrode_size, bounds, model):
       # print('score', score)
       scores.append(score)
   
-  return np.array(scores), n_eff
-
+  return np.array(scores)
 
 def particle_swarm(num_electrodes, electrode_size, num_particles, iterations, bounds, overlapBounds, model, experiments, loss_func):
     '''
@@ -122,25 +128,30 @@ def particle_swarm(num_electrodes, electrode_size, num_particles, iterations, bo
     '''
     
     curr_experiment = experiments[num_electrodes][electrode_size][loss_func]
-
-    # Parameters of how particles move
-    options = {'c1': 2.3, 'c2': 1.9, 'w': 1.6}
     
     # Initialize the swarm
+    options = {'c1': 2.3, 'c2': 1.9, 'w': 1.6}    
     swarm = P.create_swarm(n_particles=num_particles, dimensions=int(num_electrodes)*2, options=options) # The Swarm Class
     my_topology = Ring() # The Topology Class
+     
+    # Initial number effective
+    init_num_eff = get_init_num_effective(swarm.position, int(electrode_size), bounds, model)
+    curr_experiment["initial_num_effective"] = init_num_eff
+    print(f"Initial Number of Effective: {init_num_eff}")   
 
     for i in range(iterations):
         # Update personal best
-        swarm.current_cost, n_eff = eval(loss_func + "(swarm.position, int(electrode_size), bounds, model)")
+        swarm.current_cost = eval(loss_func + "(swarm.position, int(electrode_size), bounds, model)")
         
-        print(swarm.current_cost.shape)
         minIndex = np.argmin(swarm.current_cost)
         
         if (swarm.current_cost[minIndex]) < swarm.best_cost:
           swarm.best_cost = swarm.current_cost[minIndex]
-          print(swarm.position.shape, minIndex)
           swarm.best_pos = swarm.position[minIndex]
+          
+          #get numnber effective at min index
+          implant = imp.build_implant(swarm.best_pos, int(electrode_size))
+          best_num_eff = imp.get_num_effective(implant, model)
                   
         # Update position and velocity matrices
         swarm.velocity = my_topology.compute_velocity(swarm)
@@ -148,19 +159,17 @@ def particle_swarm(num_electrodes, electrode_size, num_particles, iterations, bo
 
         overlapOverWholeSwarm(swarm.position, overlapBounds)
         
-        # if i % 10 == 0:
-        #     print(f"[Iteration {i}] Best Cost: {swarm.best_cost}")
         curr_experiment["best_cost_iterations"].append(int(swarm.best_cost))
     
     curr_experiment["best_cost"] = int(swarm.best_cost)
     curr_experiment["best_positions"] = list(swarm.best_pos)
-    curr_experiment["best_num_eff"] = int(n_eff)
+    curr_experiment["best_num_eff"] = int(best_num_eff)
     
     with open('data.json', 'w') as data:
       json.dump(experiments, data)
       print("Data JSON Updated")
       
-    return swarm.best_pos, swarm.best_cost, n_eff 
+    return swarm.best_pos, swarm.best_cost, best_num_eff 
 
 def distance_penalty(ePositions, penalty_dist, less_than):
   coords = ePositions.copy()
